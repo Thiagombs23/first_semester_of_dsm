233b3797cf134510a1b0b6fae30152c8d91b8b7b
Thiago Brito <126209617+Thiagombs23@users.noreply.github.com>
2023-10-02 21:10:33 -0300

    Front made with React

---
+++ Code_front_React/src/App.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import Entrada from "./components/Entrada";
+import Lista from "./Lista";
+import Contexto from "./Contexto";
+
+export default function App() {
+  const [lista, setLista] = useState([] as string[]);
+
+  function add(nome: string) {
+    const temp = [...lista, nome];
+    setLista(temp);
+  }
+  return (
+    <Contexto.Provider value={{lista,add}}>
+      <Entrada  />
+     <Lista  />
+    </Contexto.Provider>
+  );
+}
